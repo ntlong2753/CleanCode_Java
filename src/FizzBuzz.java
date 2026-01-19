9062c46724cd74283097ddebbde62c3b0f420240
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-01-19 09:18:50 +0700

    add

---
+++ src/FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if(isFizz && isBuzz) {
+            return "FizzBuzz";
+        }
+        if(number % 3 == 0) {
+            return "Fizz";
+        }
+        if(number % 5 == 0) {
+            return "Buzz";
+        }
+        return number + "";
+    }
+}
